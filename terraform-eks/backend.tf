e6ab1d7b4f1ad2f2ab0302a5014efbed6535a9f8
Vineeth <113631587+vineethsankre@users.noreply.github.com>
2026-02-23 13:53:50 +0530

    Update backend.tf

--- terraform-eks/backend.tf
+++ terraform-eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-backend-bucket-snake-game"
+    bucket         = "vineeth-s3-snake-game"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
