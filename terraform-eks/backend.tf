847b8f0dd8c4ca0ad6f5260e9dcd4e2eae731d32
Vineeth <113631587+vineethsankre@users.noreply.github.com>
2026-02-23 15:20:07 +0530

    Rename S3 bucket for Terraform backend

--- terraform-eks/backend.tf
+++ terraform-eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "vineeth-s3-snake-game"
+    bucket         = "vineeth-s3-bucket-snake-game"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
     use_lockfile   = true
